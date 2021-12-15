683bf4b062c1a76c8797089b35a82803d63d7d4c
AbdulMuheeth <muheeth17@gmail.com>
2021-12-16 00:23:26 +0530

    new event

--- models/event.js
+++ models/event.js
@@ -3,25 +3,25 @@ const mongoose = require('mongoose');
 const eventSchema = mongoose.Schema({
     name:{type:String,required:true},
     description:{type:String,required:true},
-    tag:{type:String,required:true,default:"this is the tagline of the event"},
+    tag:{type:String,default:"this is the tagline of the event"},
     guests:[{
-        guestname : {type:String,required:true,default:"None"},
-        guestimage : {type:String,required:true,default:"https://images.all-free-download.com/images/graphicwebp/hero_kid_icon_superman_costume_cute_cartoon_character_6845147.webp"},
-        guestfield:{type:String,required:true},
+        guestname : {type:String},
+        guestimage : {type:String},
+        guestfield:{type:String},
         profile: {
-            facebook:{type:String,required:true,default:"None"},
-            instagram : {type:String,required:true,default:"None"},
-            twitter : {type:String,required:true,default:"None"},
+            facebook:{type:String,default:"None"},
+            instagram : {type:String,default:"None"},
+            twitter : {type:String,default:"None"},
         }
     }],
     location: {type:String,required:true,default:"OAT"},
     duration : {
-        start : {type:Date,required:true,default:Date.now},
+        start : {type:Date,required:true},
         end : {type:Date,required:true}
     } ,
     images:{
-        profile:{type:String,required:true,default:"/images/vvit_campus_top.jpeg"},
-        gallery:[{type:String,required:true,default:"None"}]
+        profile:{type:String,default:"/images/vvit_campus_top.jpeg"},
+        gallery:[{type:String}]
     },
 
 })
--- routes/events.js
+++ routes/events.js
@@ -2,30 +2,100 @@ const express = require('express');
 const router = express.Router();
 const Event = require('../models/event');
 
-router.get('/',(req,res)=>{
-    
-    // const event = new Event ({
-    //     name: "viva vvit",
-    //     description: "the fest has come ",
-    //     duration:{
-    //         start: Date.now(),
-    //         end: 24/2/2021,
-    //     },
-    //     guests:{
-    //         guestname:"Hero",
-    //         guestfield:"Actor"
-    //     }
-    // })
 
-    // event.save(()=>{
-    //     console.log("event saved");
-    // })
-    // User.find({"active" : {$ne:true}},(err,foundUser)=>{
+router.get('/newevent',(req,res)=>{
+    res.render('events/eventform');
+})
+
+router.post('/newevent',(req,res)=>{
+
+
+    const guest_arr = []
+
+    if(typeof(req.body.guestName) === 'object')
+    {
+        for(var i=0;i< Object.keys(req.body.guestName).length ;i++)
+        {
+            const guest_obj = {}
+            guest_obj.guestname = req.body.guestName[Object.keys(req.body.guestName)[i]];
+            guest_obj.guestimage = req.body.guestImage[Object.keys(req.body.guestImage)[i]];
+            guest_obj.guestfield = req.body.guestProfession[Object.keys(req.body.guestProfession)[i]];
+            guest_obj.profile = {
+                facebook : req.body.facebook[Object.keys(req.body.facebook)[i]],
+                instagram : req.body.instagram[Object.keys(req.body.instagram)[i]],
+                twitter :req.body.twitter[Object.keys(req.body.twitter)[i]]
+            }
+            guest_arr.push(guest_obj);
+        }
+    }
+    else if( typeof(req.body.guestName) === 'string' )
+    {
+        
+        if(req.body.guestName != '')
+        {    
+            const guest_obj = {}    
+            guest_obj.guestname = req.body.guestName;
+            guest_obj.guestimage = req.body.guestImage;
+            guest_obj.guestfield = req.body.guestProfession;
+            guest_obj.profile = {
+                facebook : req.body.facebook,
+                instagram : req.body.instagram,
+                twitter :req.body.twitter
+            }
+            guest_arr.push(guest_obj);
+        }   
+    }
+
+
+    const image_gallery_arr = []
+
+    if(typeof(req.body.image) === 'object')
+    {
+        for(var i=0;i< Object.keys(req.body.image).length ;i++)
+        {
+            if(req.body.image[Object.keys(req.body.image)[i]] != '')
+                image_gallery_arr.push(req.body.image[Object.keys(req.body.image)[i]]);
+        }
+    }
+    else if( typeof(req.body.guestName) === 'string' )
+    {
+        if(req.body.image != '')
+            image_gallery_arr.push(req.body.image); 
+    }
+
+    
+    const event =  new Event({
+        name:req.body.eventName,
+        description:req.body.eventDescription,
+        tag:req.body.tag,
+        guests:guest_arr,
+        location: req.body.location,
+        duration : {
+            start:new Date(req.body.startDate),
+            end: new Date(req.body.endDate)
+        } ,
+        images:{
+            profile:req.body.profileImage,
+            gallery:image_gallery_arr
+        },
+    })
+
+    event.save((err)=>{
+        if(err)
+        {
+            console.log(err);
+            return;
+        }
+        res.redirect('/');
+    })
+    
+})
+
+
+router.get('/',(req,res)=>{
+
     Event.find({},(err,foundEvents) => {
         res.render('events/events',{events : foundEvents});
-        // console.log(foundEvents);
-        // res.send("hi there");
-
     });
 })
 
@@ -37,5 +107,9 @@ router.get('/:id',(req,res)=>{
     })
     
 })
+
+
+
+
 
 module.exports = router
